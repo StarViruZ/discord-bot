2ec075f48ecbca4fd8b9259b5c73edaa6143ef2e
mysticalcreature79 <77720540+mysticalcreature79@users.noreply.github.com>
2021-05-22 21:11:03 -0700

    changed alot of files

---
+++ events/client/ready.js
@@ -0,0 +1,3 @@
+module.exports =() =>{
+    console.log('your mom is alive')
+}
--- main.js
+++ main.js
@@ -1,60 +1,23 @@
 const config = require('./config.json');
-
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
-
-const prefix = '$';
-
 const fs = require('fs');
+require('dotenv').config();
 
 client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+client.on("ready", () => {
+    client.user.setPresence({ 
+         activity: 
+         { name: 'ok' }, 
+         status: 'dnd' })  
+     });
 
-for(const file of commandFiles) {
-    const cmd = require(`./commands/${file}`);
-
-    client.commands.set(cmd.name, cmd);
-}
-
-client.once('ready', () => {
-    console.log('Your mom is alive');
-});
-
-client.on('message', msg => {
-    if (msg.content.toLowerCase() == 'ok' && !msg.author.bot) {
-        client.commands.get('ok').execute(msg);
-        return;
-    }
-    if(!msg.content.startsWith(prefix) || msg.author.bot)
-        return;
-    
-    const args = msg.content.slice(prefix.length).split(/ +/);
-    const cmd = args.shift().toLowerCase();
-
-    
-        if (cmd === 'ping')
-            msg.channel.send('Fuck you');
-        else if (cmd == 'pingabitch')
-            client.commands.get('pingabitch').execute(msg);
-        else if (cmd == 'clown')
-            client.commands.get('clown').execute(msg, args);
-        else if (cmd == 'mlg')
-            client.commands.get('mlg-embed').execute(msg, args, Discord);
-        else if (cmd == 'delet')
-            client.commands.get('delet').execute(msg, args);
-        else if (cmd == 'yeet')
-            client.commands.get('kick').execute(msg, args);
-        else if (cmd == 'bonk')
-            client.commands.get('ban').execute(msg, args);
-        else if (cmd == 'shut')
-            client.commands.get('mute').execute(msg, args);
+['command_handler', 'event_handler'].forEach(handler =>{
+    require(`./handlers/${handler}`)(client, Discord);
 
 });
-
-
 // -- Login -- //
-
-client.login(config.token);
+client.login(process.env.DISCORD_TOKEN);
 // client.destroy();
