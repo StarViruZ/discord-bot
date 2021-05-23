3a87f30c8d10b95c014f21c6c99aebfa84d8291d
mysticalcreature79 <77720540+mysticalcreature79@users.noreply.github.com>
2021-05-22 21:50:02 -0700

    ok

--- commands/pingabitch.js
+++ commands/pingabitch.js
@@ -3,7 +3,7 @@ const { Message } = require("discord.js");
 module.exports = {
     name: 'pingabitch',
     description: 'ping that mf of lofty',
-    execute(msg) {
+    async execute(client, msg, args, cmd, Discord) {
 
         if (msg.member.roles.cache.some(r => { return r.name === "i identify as a clown 🤡"; }))
             msg.channel.send('<@547209027998842882> mf');
