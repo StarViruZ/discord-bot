549fa1e7bb039e8c3009818d652cc794fcbd589b
DankViruZ <42785704+StarViruZ@users.noreply.github.com>
2022-05-06 11:48:52 -0500

    ok

--- commands/createChannel.js
+++ commands/createChannel.js
@@ -4,13 +4,6 @@ module.exports = {
     permissions: ["MANAGE_CHANNELS", "SEND_MESSAGES", "ADMINISTRATOR"],
     description: 'destruction makes creation',
     execute(msg, args, cmd, client, Discord) {
-        const member = msg.mentions.users.first();
-
-        if (member) {
-            const target = msg.guild.members.cache.get(member.id);
-            target.ban();
-            msg.channel.send('User <@' + member.id + '> has been bonked for dumbo! Pog.');
-        } else
-            msg.reply('beaned invisible man lessssssssss gooooo');
+        
     }
 }
